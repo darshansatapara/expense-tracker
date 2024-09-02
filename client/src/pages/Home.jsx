b11fb4ec643464f4931ea405b74e81cf6e9155fa
Jay <jay1904prajapati@gmail.com>
2024-09-02 11:56:17 +0530

    client : src folder structure maintained

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,29 @@
 import React from "react";
+import Sidebar from "../../../src/components/Sidebar";
+import Navbar from "../../../src/components/Navbar";
+import "../css/Home.css"; // Ensure you have this file for custom styling
+import Section from "../../../src/components/Section";
+import ProfileButton from "../../../src/components/ProfileButton";
 
-export default function Home() {
-  return <div>Home</div>;
-}
+const HomePage = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <div className="row ">
+          <div className="">
+            <Navbar name="Home" />
+            {/* <ProfileButton /> */}
+          </div>
+          <div className="col-md-3 col-lg-2">
+            <Sidebar />
+          </div>
+         <div className="">
+          <Section />
+         </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
